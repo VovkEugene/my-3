4a115cde69c4f6e0694392fd6038353e21f38346
Eugene Vovk <evgeniyvovk2009@yandex.ru>
2024-02-12 00:40:57 +0300

    add Projects section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import "./App.css";
 import hero from "./assets/hero/01.jpg";
 import heroBg from "./assets/hero/02.jpg";
+import project1 from "./assets/projects/01.jpg";
+import project2 from "./assets/projects/02.jpg";
+import project3 from "./assets/projects/03.jpg";
+import project4 from "./assets/projects/05.jpg";
 
 function App() {
   return (
@@ -42,8 +46,9 @@ function App() {
         </div>
       </header>
       <main>
+        {/*--------- Hero section --------*/}
         <section>
-          <div className="container m-auto px-4 py-10 flex gap-6">
+          <div className="container m-auto mb-10 px-4 py-10 flex justify-center gap-10">
             <div>
               <h1 className="font-bold text-4xl mb-1">
                 Hello, I'm
@@ -78,6 +83,96 @@ function App() {
                 alt="Hero image"
                 className="absolute top-10 left-12 z-0 rounded-xl"
               />
+            </div>
+          </div>
+        </section>
+        {/*--------- Projects section -------*/}
+        <section>
+          <div className="container m-auto px-4">
+            <h2 className="text-2xl font-semibold">Projects</h2>
+            <div className="flex flex-col sm:flex-row gap-10 mt-11">
+              <article className="border border-slate-500 rounded-md p-5 flex-1">
+                <img src={project1} alt="First project image" />
+                <h3 className="text-2xl font-semibold mt-8 mb-2">
+                  First project
+                </h3>
+                <p className="text-sm text-gray-400">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Necessitatibus non quisquam facilis tenetur nesciunt, nobis
+                  culpa accusamus, numquam laudantium quia dolores nulla
+                  deleniti. Laborum nam earum non tempora voluptates est.
+                </p>
+                <div className="flex gap-2 mt-12">
+                  <button className="px-8 py-3 border border-slate-500 rounded-lg bg-gradient-to-t from-cyan-900 to-red-300 hover:from-blue-900 hover:to-red-200">
+                    Live preview
+                  </button>
+                  <button className="px-8 py-3 border rounded-lg hover:border-slate-400 hover:text-slate-400">
+                    Checkout github
+                  </button>
+                </div>
+              </article>
+              <article className="border border-slate-500 rounded-md p-5 flex-1">
+                <img src={project2} alt="Second project image" />
+                <h3 className="text-2xl font-semibold mt-8 mb-2">
+                  Second project
+                </h3>
+                <p className="text-sm text-gray-400">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Quaerat delectus accusantium laudantium officia quasi voluptas
+                  aspernatur at tempora saepe ea ipsa doloremque adipisci neque
+                  dolorum atque, repellat optio voluptates architecto.
+                </p>
+                <div className="flex gap-2 mt-12">
+                  <button className="px-8 py-3 border border-slate-500 rounded-lg bg-gradient-to-t from-cyan-900 to-red-300 hover:from-blue-900 hover:to-red-200">
+                    Live preview
+                  </button>
+                  <button className="px-8 py-3 border rounded-lg hover:border-slate-400 hover:text-slate-400">
+                    Checkout github
+                  </button>
+                </div>
+              </article>
+            </div>
+            <div className="flex flex-col sm:flex-row gap-10 mt-11">
+              <article className="border border-slate-500 rounded-md p-5 flex-1">
+                <img src={project3} alt="Third project image" />
+                <h3 className="text-2xl font-semibold mt-8 mb-2">
+                  Third project
+                </h3>
+                <p className="text-sm text-gray-400">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Officiis provident cumque natus eligendi consequatur officia
+                  unde atque molestias voluptatum. Porro aperiam quidem beatae
+                  quas? Quia vel obcaecati soluta itaque explicabo.
+                </p>
+                <div className="flex gap-2 mt-12">
+                  <button className="px-8 py-3 border border-slate-500 rounded-lg bg-gradient-to-t from-cyan-900 to-red-300 hover:from-blue-900 hover:to-red-200">
+                    Live preview
+                  </button>
+                  <button className="px-8 py-3 border rounded-lg hover:border-slate-400 hover:text-slate-400">
+                    Checkout github
+                  </button>
+                </div>
+              </article>
+              <article className="border border-slate-500 rounded-md p-5 flex-1">
+                <img src={project4} alt="Fourth project image" />
+                <h3 className="text-2xl font-semibold mt-8 mb-2">
+                  Fourth project
+                </h3>
+                <p className="text-sm text-gray-400">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Illum, ex optio. Numquam aut nisi reprehenderit quas odit sint
+                  ratione molestiae, amet accusamus natus sit earum dolorem
+                  dolores qui enim officiis.
+                </p>
+                <div className="flex gap-2 mt-12">
+                  <button className="px-8 py-3 border border-slate-500 rounded-lg bg-gradient-to-t from-cyan-900 to-red-300 hover:from-blue-900 hover:to-red-200">
+                    Live preview
+                  </button>
+                  <button className="px-8 py-3 border rounded-lg hover:border-slate-400 hover:text-slate-400">
+                    Checkout github
+                  </button>
+                </div>
+              </article>
             </div>
           </div>
         </section>
