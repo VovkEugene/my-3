aee377f66ff41f4e978fa8150425c7a6b2833675
Eugene Vovk <evgeniyvovk2009@yandex.ru>
2024-03-03 22:57:05 +0300

    create AdditionalSkills section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import AdditionalSkills from "./components/AdditionalSkills";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
@@ -12,6 +13,7 @@ function App() {
         <Hero />
         <Projects />
         <Technologies />
+        <AdditionalSkills />
       </main>
     </>
   );
---
+++ src/components/AdditionalSkills.jsx
@@ -0,0 +1,20 @@
+const AdditionalSkills = () => {
+  return (
+    <section>
+      <div className="container m-auto py-14 px-8">
+        <h2 className="text-2xl font-semibold">
+          Additional technologies and skills
+        </h2>
+        <div className="flex flex-col sm:flex-row gap-4 sm:gap-28 mt-12 w-[80%]">
+          <div>
+            <p className="relative left-5 font-bold before:absolute before:w-4 before:h-4 before:block before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:rounded-full before:mt-1 before:-left-6 ">
+              Git
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AdditionalSkills;
